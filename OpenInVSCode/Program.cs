49e4eaf89f805cd47a011623489eb478fef94576
mohemohe <mohemohe@ghippos.net>
2015-05-08 01:21:49 +0900

    🍣

---
+++ OpenInVSCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace OpenInVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cd = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var arg = "--processStart Code.exe -a=";
+            foreach (var s in args)
+            {
+                arg += "\"" + s + "\" ";
+            }
+
+            Directory.SetCurrentDirectory(cd);
+            Process.Start("Update.exe", arg);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # OpenInVSCode
 Open File in Visual Studio Code
+
+[download](https://github.com/mohemohe/OpenInVSCode/releases)
+
+please put the "OpenInVSCode.exe" in the same location to the "Update.exe".  
+(probably to "AppData\Local\Code")
